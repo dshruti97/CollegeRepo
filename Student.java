db76dcbdd4a2ff2af01453c7f6e72a74a088e5fc
Shruti Desai <shruti.desai@ibm.com>
2022-06-06 11:38:56 +0530

    Added student and teacher

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+  public static void main(String args[]) {
+    String name;
+    String college;
+
+    public void introduction() {
+      System.out.println("My name is " + name " from " + college);
+    }
+  }
+}
---
+++ Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher {
+  public static void main(String args[]) {
+    String name;
+    String subject;
+
+    public void introduction() {
+      System.out.println("My name is " + name + " I teach" + subject);
+    }
+  }
+}
