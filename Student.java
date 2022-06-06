23fada227edc6123f02235ccf16b48e342f273e2
Shruti Desai <shruti.desai@ibm.com>
2022-06-06 12:14:41 +0530

    Modified student and teacher

--- Student.java
+++ Student.java
@@ -2,9 +2,10 @@ public class Student {
   public static void main(String args[]) {
     String name;
     String college;
+    String city;
 
     public void introduction() {
-      System.out.println("My name is " + name " from " + college);
+      System.out.println("My name is " + name " from " + college + " in " + city);
     }
   }
 }
--- Teacher.java
+++ Teacher.java
@@ -2,9 +2,10 @@ public class Teacher {
   public static void main(String args[]) {
     String name;
     String subject;
+    String college;
 
     public void introduction() {
-      System.out.println("My name is " + name + " I teach" + subject);
+      System.out.println("My name is " + name + " I teach" + subject + " at " + college);
     }
   }
 }
